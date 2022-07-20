dd1a84f875eebf3d89b51301c33b8ba5f91fd7c1
Kory Smith <kory.smith@kroger.com>
2022-07-20 14:36:15 -0400

    adds click handlers to buttons and backdrop that let us open and close the modal

--- shared.js
+++ shared.js
@@ -1,5 +1,22 @@
-const backdrop = document.querySelector(".backdrop")
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const selectPlanButtons = document.querySelectorAll("article.plan button");
 
-// console.dir(backdrop)
+for (const button of selectPlanButtons) {
+  button.addEventListener("click", function (event) {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
 
-backdrop.style.display = "block"
+const noButton = document.querySelector(".modal__action--negative")
+
+noButton.addEventListener("click", function(event) {
+	modal.style.display = "none";
+	backdrop.style.display = "none";
+})
+
+backdrop.addEventListener("click", function(event) {
+	modal.style.display = "none";
+	backdrop.style.display = "none";
+})
