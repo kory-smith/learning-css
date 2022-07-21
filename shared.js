884549e356b136b6ff635b1c367595bb45f26781
Kory Smith <kory.smith@kroger.com>
2022-07-21 11:07:48 -0400

    adds a toggle functionality to the mobile hamburger menu

--- shared.js
+++ shared.js
@@ -1,6 +1,8 @@
 const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanButtons = document.querySelectorAll("article.plan button");
+const toggleButton = document.querySelector(".toggle-button")
+const mobileNav = document.querySelector(".mobile-nav")
 
 for (const button of selectPlanButtons) {
   button.addEventListener("click", function (event) {
@@ -14,9 +16,16 @@ const noButton = document.querySelector(".modal__action--negative")
 noButton.addEventListener("click", function(event) {
 	modal.style.display = "none";
 	backdrop.style.display = "none";
+	mobileNav.style.display = "none";
 })
 
 backdrop.addEventListener("click", function(event) {
 	modal.style.display = "none";
 	backdrop.style.display = "none";
+	mobileNav.style.display = "none";
+})
+
+toggleButton.addEventListener("click", function() {
+	mobileNav.style.display = "block";
+	backdrop.style.display = "block";
 })
